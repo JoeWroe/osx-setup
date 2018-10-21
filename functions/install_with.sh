439c99f3f018660ad8be0993fed924bf27b5dcac
Joe Wroe <joe_wroe@icloud.com>
2018-10-21 01:01:46 +0100

    Add #install_with

---
+++ functions/install_with.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+function install_with {
+    if [ "$current_user_input" = "$yes" ];
+    then
+        $1;
+    fi
+}
--- osx-setup.sh
+++ osx-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ./functions/ask_to_install.sh
+source ./functions/install_with.sh
 
 yes="y";
 no="n";
@@ -9,33 +10,17 @@ current_user_input="";
 
 echo ""
 read -p "Would you like to setup a public/private key pair? Please input y/n.  " current_user_input;
+install_with 'ssh-keygen -t rsa -b 4096 -C "joe_wroe@icloud.com"'
 
-if [ "$current_user_input" = "$yes" ];
-then
-    ssh-keygen -t rsa -b 4096 -C "joe_wroe@icloud.com"
-fi
+ask_to_install 'Xcode'
+install_with 'xcode-select --install'
 
-ask_to_install "Xcode";
-
-if [ "$current_user_input" = "$yes" ];
-then
-    xcode-select --install;
-fi
-
-ask_to_install "Homebrew";
-
-if [ "$current_user_input" = "$yes" ];
-then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-    brew update;
-fi
+ask_to_install 'Homebrew'
+install_with 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+brew update
 
 ask_to_install "Ruby Version Manager (RVM)"
+install_with "curl -L https://get.rvm.io | bash -s stable"
 
-if [ "$current_user_input" = "$yes" ];
-then
-    \curl -L https://get.rvm.io | bash -s stable;
-fi
-
-echo ''
-echo 'Setup Finished!'
+echo ""
+echo "Setup Finished!"
