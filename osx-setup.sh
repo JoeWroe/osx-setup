bfa7866c329a6e29a527f1b57f591ec2eb26e299
Joe Wroe <joe_wroe@icloud.com>
2018-10-22 20:31:35 +0100

    Add install for RVM default Ruby version
    ∆ Required a login shell, done at top after /bin/bash.

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash --login
 
 for file in ./functions/*; do . $file; done
 
@@ -16,6 +16,9 @@ check_if_user_input_yes && brew update
 
 ask_to 'install' 'Ruby Version Manager (RVM)'
 check_if_user_input_yes && curl -L https://get.rvm.io | bash -s stable && source /Users/joe-wroe/.rvm/scripts/rvm
+
+ask_to 'install' "RVM's default Ruby version"
+check_if_user_input_yes && rvm use ruby --install --default && ruby -v
 
 echo ''
 echo 'Setup Finished!'
