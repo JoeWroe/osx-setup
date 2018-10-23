fa63ea858a4e65b6d853e3580e9f8ebe5d2cf06b
Joe Wroe <joe_wroe@icloud.com>
2018-10-23 23:23:44 +0100

    Refactor Ruby tools into a set

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --login
 
-for file in ./functions/*; do . $file; done
+for file in ./helpers/*; do . $file; done
+for tool_set in ./tool-sets/*; do . $tool_set; done
 
 ask_to 'setup' 'a public/private key pair'
 check_if_user_input_yes && ssh-keygen -t rsa -b 4096 -C "joe_wroe@icloud.com"
@@ -14,14 +15,8 @@ check_if_user_input_yes && ruby -e "$(curl -fsSL https://raw.githubusercontent.c
 ask_to 'update' 'brew'
 check_if_user_input_yes && brew update
 
-ask_to 'install' 'Ruby Version Manager (RVM)'
-check_if_user_input_yes && curl -L https://get.rvm.io | bash -s stable && source /Users/joe-wroe/.rvm/scripts/rvm
-
-ask_to 'install' "RVM's default Ruby version"
-check_if_user_input_yes && rvm use ruby --install --default && ruby -v
-
-ask_to 'install' 'PRY'
-check_if_user_input_yes && gem install pry
+ask_to 'setup' 'Ruby related tools'
+check_if_user_input_yes && install_ruby_related_deps
 
 ask_to 'install' 'the Heroku Toolbelt'
 check_if_user_input_yes && brew install heroku/brew/heroku
