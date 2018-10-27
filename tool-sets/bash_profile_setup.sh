3d550a9042ea2b09ddd6461ad49aa72abee4f68b
Joe Wroe <joe_wroe@icloud.com>
2018-10-27 23:22:24 +0100

    Add a friendly command prompt

--- tool-sets/bash_profile_setup.sh
+++ tool-sets/bash_profile_setup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 function setup_bash_profile {
-    ask_to 'add' 'up arrow bash history search'
-    check_if_user_input_yes && cat <<'EOF' >>~/.bash_profile
+    ask_to 'add' 'a friendly command prompt'
+    check_if_user_input_yes && cat <<'    EOF' >>~/.bash_profile
 
-# Up arrow bash history search
-bind '"\e[A":history-search-backward'
-EOF
+    # A friendly command prompt
+    export PS1='\[\e[41m\] \[\e[40m\]\[\e[1;37m\] Hi, \u you are in \w, it is currently \@ \n\[\e[41m\] \[\e[40m\] ~>\[\e[0m\] '
+    export PS2='\[\e[41m\] \[\e[40m\]\[\e[1;37m\] cont. ~> \[\e[0m\]'
+    EOF
+
+    ask_to 'add' 'up arrow bash history search'
+    check_if_user_input_yes && cat <<'    EOF' >>~/.bash_profile
+
+    # Up arrow bash history search
+    bind '"\e[A":history-search-backward'
+    EOF
 
     ask_to 'add' 'down arrow bash history search'
-    check_if_user_input_yes && cat <<'EOF' >>~/.bash_profile
+    check_if_user_input_yes && cat <<'    EOF' >>~/.bash_profile
 
-# Down arrow bash history search
-bind '"\e[B":history-search-forward'
-EOF
+    # Down arrow bash history search
+    bind '"\e[B":history-search-forward'
+    EOF
 
     source ~/.bash_profile
 }
