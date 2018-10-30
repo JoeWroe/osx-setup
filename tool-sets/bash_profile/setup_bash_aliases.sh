1e17860281eacd5241263b3da6e25fb78b6be85e
Joe Wroe <joe_wroe@icloud.com>
2018-10-30 12:50:55 +0000

    Add ascii art header to bash_profile aliases

--- tool-sets/bash_profile/setup_bash_aliases.sh
+++ tool-sets/bash_profile/setup_bash_aliases.sh
@@ -1,4 +1,17 @@
 function setup_bash_aliases {
+        ask_to 'add an ascii art header to bash aliases section'
+    check_if_user_input_yes && cat <<'    EOF' >>~/.bash_profile
+
+    # 
+    #      _/_/_/      _/_/      _/_/_/  _/    _/        _/_/    _/        _/_/_/    _/_/      _/_/_/  _/_/_/_/    _/_/_/   
+    #     _/    _/  _/    _/  _/        _/    _/      _/    _/  _/          _/    _/    _/  _/        _/        _/          
+    #    _/_/_/    _/_/_/_/    _/_/    _/_/_/_/      _/_/_/_/  _/          _/    _/_/_/_/    _/_/    _/_/_/      _/_/       
+    #   _/    _/  _/    _/        _/  _/    _/      _/    _/  _/          _/    _/    _/        _/  _/              _/      
+    #  _/_/_/    _/    _/  _/_/_/    _/    _/      _/    _/  _/_/_/_/  _/_/_/  _/    _/  _/_/_/    _/_/_/_/  _/_/_/         
+    #
+
+    EOF
+
     ask_to 'add up arrow bash history search'
     check_if_user_input_yes && cat <<'    EOF' >>~/.bash_profile
     bind '"\e[A":history-search-backward'        # Up arrow bash history search
