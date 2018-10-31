c13188f901669dfc964a59011320a4f9692b9640
Joe Wroe <joe_wroe@icloud.com>
2018-10-31 19:45:14 +0000

    Refactor install Ruby deps to use a crude implementation of a menu installer

--- tool-sets/ruby_related_deps.sh
+++ tool-sets/ruby_related_deps.sh
@@ -1,4 +1,28 @@
 function install_ruby_related_deps {
+
+	while [ $current_user_input != exit ]; do
+		read -d '' current_options <<"		EOV"
+	
+		Please select which Ruby related dependency you would like to install:
+
+		∆ Ruby Version Manager (RVM)
+		∆ RVM's default Ruby version
+		∆ Pry - A Ruby REPL that is a nice alternative to IRB.
+		∆ exit
+
+		EOV
+
+		echo "$current_options"
+
+		read current_user_input
+
+		[[ $current_user_input = Pry ]] && gem install pry
+	done
+
+
+
+    # ---- working stuff ----
+
     ask_to 'install Ruby Version Manager (RVM)'
     check_if_user_input_yes && curl -L https://get.rvm.io | bash -s stable && source /Users/joe-wroe/.rvm/scripts/rvm
 
