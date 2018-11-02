cc455e17860b57065881d507213ed6f315b5a6a0
Joe Wroe <joe_wroe@icloud.com>
2018-11-02 17:51:05 +0000

    Add menu to bash_profile setup

--- tool-sets/bash_profile_setup.sh
+++ tool-sets/bash_profile_setup.sh
@@ -1,12 +1,27 @@
 for file in ./tool-sets/bash_profile/*.sh; do . $file; done
 
 function setup_bash_profile {
+red_text=$(tput setaf 1)
+yellow_text=$(tput setaf 3)
+white_text=$(tput setaf 7)
 
-    ask_to 'add sugar to bash'
-    check_user_input yes && setup_bash_sugar 
+	while [ $current_user_input != exit ]; do
+		read -d '' current_options <<-EOV
 
-    ask_to 'add preferred aliases'
-    check_user_input yes && setup_bash_aliases
+		Please select what you would like to add to your bash profile. (type the phrase in ${red_text}red${white_text}):
 
-    source ~/.bash_profile
+		${yellow_text}∆ ${red_text}Bash sugar${white_text}
+		${yellow_text}∆ ${red_text}Bash aliases${white_text}
+
+		EOV
+
+		echo "$current_options"
+
+		read current_user_input
+
+		check_user_input "Bash sugar" && setup_bash_sugar
+		check_user_input "Bash aliases" && setup_bash_aliases
+	done
+
+	source ~/.bash_profile
 }
