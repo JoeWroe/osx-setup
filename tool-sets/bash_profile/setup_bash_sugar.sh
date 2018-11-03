cada4da901eeaf4f238fe0f69494772eb2dc595c
Joe Wroe <joe_wroe@icloud.com>
2018-11-03 20:58:50 +0000

    Add menu selector to bash_sugar

--- tool-sets/bash_profile/setup_bash_sugar.sh
+++ tool-sets/bash_profile/setup_bash_sugar.sh
@@ -1,32 +1,60 @@
 function setup_bash_sugar {
-    ask_to 'add' 'an ascii art header to bash sugar section'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
+red_text=$(tput setaf 1)
+yellow_text=$(tput setaf 3)
+white_text=$(tput setaf 7)
 
-    #
-    #      _/_/_/      _/_/      _/_/_/  _/    _/        _/_/_/  _/    _/    _/_/_/    _/_/    _/_/_/    
-    #     _/    _/  _/    _/  _/        _/    _/      _/        _/    _/  _/        _/    _/  _/    _/   
-    #    _/_/_/    _/_/_/_/    _/_/    _/_/_/_/        _/_/    _/    _/  _/  _/_/  _/_/_/_/  _/_/_/      
-    #   _/    _/  _/    _/        _/  _/    _/            _/  _/    _/  _/    _/  _/    _/  _/    _/     
-    #  _/_/_/    _/    _/  _/_/_/    _/    _/      _/_/_/      _/_/      _/_/_/  _/    _/  _/    _/      
-    #
+	while [ $current_user_input != exit ]; do
+		read -d '' current_options <<-EOV
 
-    EOF
+		Please select which bash profile sugar you would like to add. (type the phrase in ${red_text}red${white_text}):
+
+		${yellow_text}∆ ${red_text}Ascii art${white_text} - add an ascii art header to the sugar part of your bash profile.
+		${yellow_text}∆ ${red_text}Command prompt${white_text} - add a friendly command prompt.
+		${yellow_text}∆ ${red_text}Coloured output${white_text} - add colour to the text output from common terminal commands.
+		${yellow_text}∆ ${red_text}Exit${white_text}
+
+		EOV
+
+		echo "$current_options"
+
+		read current_user_input
+
+		check_user_input "Ascii art" && add_ascii_art_sugar_header
+		check_user_input "Command prompt" && add_command_prompt
+		check_user_input "Coloured output" && add_coloured_terminal_output
+	done
+}
+
+function add_ascii_art_sugar_header {
+	cat <<-EOF >>~/.bash_profile
+
+		#
+		#      _/_/_/      _/_/      _/_/_/  _/    _/        _/_/_/  _/    _/    _/_/_/    _/_/    _/_/_/    
+		#     _/    _/  _/    _/  _/        _/    _/      _/        _/    _/  _/        _/    _/  _/    _/   
+		#    _/_/_/    _/_/_/_/    _/_/    _/_/_/_/        _/_/    _/    _/  _/  _/_/  _/_/_/_/  _/_/_/      
+		#   _/    _/  _/    _/        _/  _/    _/            _/  _/    _/  _/    _/  _/    _/  _/    _/     
+		#  _/_/_/    _/    _/  _/_/_/    _/    _/      _/_/_/      _/_/      _/_/_/  _/    _/  _/    _/      
+		#
+
+	EOF
+}
                                                                                               
-    ask_to 'add' 'a friendly command prompt'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
+function add_command_prompt {
+	cat <<'        EOF' >>~/.bash_profile
 
-    # A friendly command prompt
-    # ------------------------------------------------
-    export PS1='\[\e[41m\] \[\e[40m\]\[\e[1;37m\] Hi, \u you are in \w, it is currently \@ \n\[\e[41m\] \[\e[40m\] ~>\[\e[0m\] '
-    export PS2='\[\e[41m\] \[\e[40m\]\[\e[1;37m\] cont. ~> \[\e[0m\]'
-    EOF
+		# A friendly command prompt
+		# ------------------------------------------------
+		export PS1='\[\e[41m\] \[\e[40m\]\[\e[1;37m\] Hi, \u you are in \w, it is currently \@ \n\[\e[41m\] \[\e[40m\] ~>\[\e[0m\] '
+		export PS2='\[\e[41m\] \[\e[40m\]\[\e[1;37m\] cont. ~> \[\e[0m\]'
+        EOF
+}
 
-    ask_to 'add' 'coloured terminal output'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
+function add_coloured_terminal_output {
+	cat <<-EOF >>~/.bash_profile
 
-    # Coloured Terminal Output
-    # ------------------------------------------------
-    export CLICOLOR=1
-    export LSCOLORS=ExFxBxDxCxegedabagacad
-    EOF
+		# Coloured Terminal Output
+		# ------------------------------------------------
+		export CLICOLOR=1
+		export LSCOLORS=ExFxBxDxCxegedabagacad
+	EOF
 }
--- tool-sets/bash_profile_setup.sh
+++ tool-sets/bash_profile_setup.sh
@@ -10,8 +10,8 @@ white_text=$(tput setaf 7)
 
 		Please select what you would like to add to your bash profile. (type the phrase in ${red_text}red${white_text}):
 
-		${yellow_text}∆ ${red_text}Bash sugar${white_text}
-		${yellow_text}∆ ${red_text}Bash aliases${white_text}
+		${yellow_text}∆ ${red_text}Sugar${white_text} - add some sugar to your terminal.
+		${yellow_text}∆ ${red_text}Aliases${white_text} - add some useful aliases to your terminal.
 
 		EOV
 
@@ -19,8 +19,10 @@ white_text=$(tput setaf 7)
 
 		read current_user_input
 
-		check_user_input "Bash sugar" && setup_bash_sugar
-		check_user_input "Bash aliases" && setup_bash_aliases
+		echo $current_user_input
+
+		check_user_input Sugar && setup_bash_sugar
+		check_user_input Aliases && setup_bash_aliases
 	done
 
 	source ~/.bash_profile
