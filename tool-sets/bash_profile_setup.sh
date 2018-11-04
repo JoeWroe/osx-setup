878e408fc651461230ede24b1f839b0223430941
Joe Wroe <joe_wroe@icloud.com>
2018-11-04 20:08:05 +0000

    Add correct quoting to bash_profile_setup

--- tool-sets/bash_profile_setup.sh
+++ tool-sets/bash_profile_setup.sh
@@ -5,22 +5,22 @@ red_text=$(tput setaf 1)
 yellow_text=$(tput setaf 3)
 white_text=$(tput setaf 7)
 
-	while [ $current_user_input != exit ]; do
+	while [ "$current_user_input" != exit ]; do
 		read -d '' current_options <<-EOV
 
 		Please select what you would like to add to your bash profile. (type the phrase in ${red_text}red${white_text}):
 
-		${yellow_text}∆ ${red_text}Sugar${white_text} - add some sugar to your terminal.
-		${yellow_text}∆ ${red_text}Aliases${white_text} - add some useful aliases to your terminal.
-
+		${yellow_text}∆ ${red_text}Bash sugar${white_text} - add some sugar to your terminal.
+		${yellow_text}∆ ${red_text}Bash aliases${white_text} - add some useful aliases to your terminal.
+		${yellow_text}∆ ${red_text}Exit${white_text}
 		EOV
 
 		echo "$current_options"
 
 		read current_user_input
 
-		check_user_input Sugar && setup_bash_sugar
-		check_user_input Aliases && setup_bash_aliases
+		check_user_input "Bash sugar" && setup_bash_sugar
+		check_user_input "Bash aliases" && setup_bash_aliases
 	done
 
 	source ~/.bash_profile
