0ae6098cb8aaf683ed3fdd8cfd1603067cc4441a
Joe Wroe <joe_wroe@icloud.com>
2018-11-05 18:52:11 +0000

    Menuify bash aliases

--- tool-sets/bash_profile/setup_bash_aliases.sh
+++ tool-sets/bash_profile/setup_bash_aliases.sh
@@ -1,29 +1,59 @@
 function setup_bash_aliases {
-        ask_to 'add an ascii art header to bash aliases section'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
+red_text=$(tput setaf 1)
+yellow_text=$(tput setaf 3)
+white_text=$(tput setaf 7)
 
-    # 
-    #      _/_/_/      _/_/      _/_/_/  _/    _/        _/_/    _/        _/_/_/    _/_/      _/_/_/  _/_/_/_/    _/_/_/   
-    #     _/    _/  _/    _/  _/        _/    _/      _/    _/  _/          _/    _/    _/  _/        _/        _/          
-    #    _/_/_/    _/_/_/_/    _/_/    _/_/_/_/      _/_/_/_/  _/          _/    _/_/_/_/    _/_/    _/_/_/      _/_/       
-    #   _/    _/  _/    _/        _/  _/    _/      _/    _/  _/          _/    _/    _/        _/  _/              _/      
-    #  _/_/_/    _/    _/  _/_/_/    _/    _/      _/    _/  _/_/_/_/  _/_/_/  _/    _/  _/_/_/    _/_/_/_/  _/_/_/         
-    #
+	while [ "$current_user_input" != exit ]; do
+		read -d '' current_options <<-EOV
 
-    EOF
+		Please select which aliases you would like to add to your bash profile. (type the phrase in ${red_text}red${white_text}):
 
-    ask_to 'add up arrow bash history search'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
-    bind '"\e[A":history-search-backward'        # Up arrow bash history search
-    EOF
+		${yellow_text}∆ ${red_text}Ascii art${white_text} - add an ascii art header to the aliases part of your bash profile.
+		${yellow_text}∆ ${red_text}Up arrow history search${white_text} - a single press of the 'up' arrow searches your bash history for the most recent, alphabetically similar command.
+		${yellow_text}∆ ${red_text}Down arrow history search${white_text} - a single press of the 'down' arrow searches your bash history for the most recent, alphabetically similar command.
+		${yellow_text}∆ ${red_text}Preferred list${white_text} - 'ls' does 'ls -FGlAhp'.
+		${yellow_text}∆ ${red_text}Exit${white_text}
+		EOV
 
-    ask_to 'add down arrow bash history search'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
-    bind '"\e[B":history-search-forward'         # Down arrow bash history search
-    EOF
+		echo "$current_options"
 
-    ask_to 'add preferred list alias'
-    check_user_input yes && cat <<'    EOF' >>~/.bash_profile
-    alias ls='ls -FGlAhp'                        # Preferred 'ls'
-    EOF
+		read current_user_input
+
+		check_user_input "Ascii art" && add_ascii_art_aliases_header
+		check_user_input "Up arrow history search" && add_up_arrow_bash_history_search
+		check_user_input "Down arrow history search" && add_down_arrow_bash_history_search
+		check_user_input "Preferred list" && add_preferred_list_alias
+	done
+}
+
+function add_ascii_art_alises_header {
+	cat <<-EOF >>~/.bash_profile
+
+		# 
+		#      _/_/_/      _/_/      _/_/_/  _/    _/        _/_/    _/        _/_/_/    _/_/      _/_/_/  _/_/_/_/    _/_/_/   
+		#     _/    _/  _/    _/  _/        _/    _/      _/    _/  _/          _/    _/    _/  _/        _/        _/          
+		#    _/_/_/    _/_/_/_/    _/_/    _/_/_/_/      _/_/_/_/  _/          _/    _/_/_/_/    _/_/    _/_/_/      _/_/       
+		#   _/    _/  _/    _/        _/  _/    _/      _/    _/  _/          _/    _/    _/        _/  _/              _/      
+		#  _/_/_/    _/    _/  _/_/_/    _/    _/      _/    _/  _/_/_/_/  _/_/_/  _/    _/  _/_/_/    _/_/_/_/  _/_/_/         
+		#
+
+	EOF
+}
+
+function add_up_arrow_bash_history_search {
+	cat <<'	EOF' >>~/.bash_profile
+	bind '"\e[A":history-search-backward'        # Up arrow bash history search
+	EOF
+}
+
+function add_down_arrow_bash_history_search {
+	cat <<'	EOF' >>~/.bash_profile
+	bind '"\e[B":history-search-forward'         # Down arrow bash history search
+	EOF
+}
+
+function add_preferred_list_alias {
+	cat <<'	EOF' >>~/.bash_profile
+	alias ls='ls -FGlAhp'                        # Preferred 'ls'
+	EOF
 }
