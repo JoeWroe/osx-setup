75efadaf28559380900f73e853f68ec13c09d632
Joe Wroe <joe_wroe@icloud.com>
2018-11-05 13:09:03 +0000

    Add contextual statement to check_user_input helper

--- helpers/check_user_input.sh
+++ helpers/check_user_input.sh
@@ -1,3 +1,7 @@
+# This function should take a single argument and checks that the argument
+# matches the current_user_input variable. It's used to confirm user input when
+# deciding which tool to install.
+
 function check_user_input {
     [[ $current_user_input = $1 ]]
 }
